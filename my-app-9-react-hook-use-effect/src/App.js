cc78950e8a7cc3357a166dd423ff07c9f329a131
Wisnu Andika <wisnuandika104@gmail.com>
2023-09-22 15:03:19 +0700

    Upload part 9

---
+++ my-app-9-react-hook-use-effect/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './pages/Counter';
+import Timers from './pages/Timers';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Timers />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app-9-react-hook-use-effect/src/pages/Counter.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+  const [calculation, setCalculation] = useState(0)
+  
+  useEffect(() => {
+    setCalculation(() => count * 5)
+  }, [count])
+
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button onClick={ () => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
+    </>  
+  )
+}
